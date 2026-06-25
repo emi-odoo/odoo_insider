b48236e847cce07517c2ae600a642ff603f4666b
emi-odoo <emi@odoo.com>
2026-06-25 10:40:52 +0200

    [UPG] label_log_message: upgrade to 19.0

--- label_log_message/__manifest__.py
+++ label_log_message/__manifest__.py
@@ -3,7 +3,7 @@
     "author": "Odoo",
     "website": "https://www.odoo.com",
     "category": "Administration",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "depends": ["web"],
     "assets": {
         "web.assets_backend": [
