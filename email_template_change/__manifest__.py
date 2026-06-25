af2134838855be4ad3ded777d4dea3d360b28f48
emi-odoo <emi@odoo.com>
2026-06-25 10:09:43 +0200

    [UPG] email_template_change: migrate module to version 19.0

--- email_template_change/__manifest__.py
+++ email_template_change/__manifest__.py
@@ -3,13 +3,15 @@
     "author": "Odoo",
     "website": "https://www.odoo.com",
     "category": "Administration",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "depends": ["sale_management"],
+    "license": "LGPL-3",
     "data": [
         # FIELDS
         "data/ir_model_fields.xml",
         # VIEWS
-        "views/email_templates.xml",
         "views/res_company_views.xml",
+        # TEMPLATES
+        "views/email_templates.xml",
     ],
 }
